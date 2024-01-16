abb71d94aae962d75f2c312c51db4d54d1217106
Amnishittechxpert <155433586+Amnishittechxpert@users.noreply.github.com>
2024-01-16 12:37:21 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,23 +1,30 @@
-
 <?php
 require("basic.php");
 ?>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="home.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="signup.php">Signup</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="login.php">Login</a>
-    </li>
-    <?php 
-    if(isset($_SESSION['users']) && $_SESSION != ''){ ?>
-    <li class="nav-item">
-      <a class="nav-link" href="logout.php">Logout</a>
-    </li>
-    <?php } ?>
-  </ul>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="home.php">Home</a>
+        </li>
+        <?php
+        if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+       
+            ?>
+            <li class="nav-item">
+                <a class="nav-link" href="logout.php">Logout</a>
+            </li>
+            <?php
+        } else {
+            ?>
+            <li class="nav-item">
+                <a class="nav-link" href="signup.php">Signup</a>
+            </li>
+            <?php
+        }
+        ?>
+        <li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+        </li>
+    </ul>
+</nav>
 </nav>
