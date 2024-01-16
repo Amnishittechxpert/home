bbb13288627b26e38cb62a1bf39fb9bf23ff63a1
Amnishittechxpert <155433586+Amnishittechxpert@users.noreply.github.com>
2024-01-16 12:37:43 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,13 +2,12 @@
 
 require("ever/header.php");
 
-
 if (!isset($_SESSION['users'])) {
     $_SESSION['users'] = array();
 }
 
 $nameErr = $lastnameErr = $emailErr = $passwordErr = "";
-$name = $lastname = $email = $password = $color = $message = "";
+$name = $lastname = $email = $password  = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["name"])) {
@@ -37,23 +36,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $emailErr = "Invalid email format";
         }
     }
-
-    if (empty($_POST["password"])) {
-        $passwordErr = "Password is required";
-    } else {
+    if(!empty($_POST["password"]) && isset( $_POST['password'] )) {
         $password = $_POST["password"];
-        $strength = checkPasswordStrength($password);
-
-        if ($strength <= 1) {
-            $color = 'red';
-            $message = 'Weak Password';
-        } elseif ($strength == 2) {
-            $color = 'orange';
-            $message = 'Medium Password';
-        } else {
-            $color = 'green';
-            $message = 'Strong Password';
+        if (mb_strlen($_POST["password"]) <= 8) {
+            $passwordErr = "Your Password Must Contain At Least 8 Characters!";
         }
+        elseif(!preg_match("#[0-9]+#",$password)) {
+            $passwordErr = "Your Password Must Contain At Least 1 Number!";
+        }
+        elseif(!preg_match("#[A-Z]+#",$password)) {
+            $passwordErr = "Your Password Must Contain At Least 1 Capital Letter!";
+        }
+        elseif(!preg_match("#[a-z]+#",$password)) {
+            $passwordErr = "Your Password Must Contain At Least 1 Lowercase Letter!";
+        }
+        elseif(!preg_match("#[\W]+#",$password)) {
+            $passwordErr = "Your Password Must Contain At Least 1 Special Character!";
+        } 
+        $password = $_POST["password"];
+        $hashedPassword = md5($password);
+       
+    } else {
+        $passwordErr = "Please enter password  ";
     }
 
     if (empty($nameErr) && empty($lastnameErr) && empty($emailErr) && empty($passwordErr)) {
@@ -63,13 +67,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($user !== false) {
             echo "User already exists";
         } else {
-            
-            $_SESSION['users'][] = array('name' => $name, 'lastname' => $lastname, 'email' => $email, 'password' => $password);
-            echo "Signup successful";
+            $_SESSION['users'][] = array('name' => $name, 'lastname' => $lastname, 'email' => $email, 'password' => $hashedPassword);
+            echo "Signup successful login Button To Countiue";
+         
+            // header("Location: login.php");  
         }
     }
 }
-
+// echo $email;
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -77,46 +82,33 @@ function test_input($data) {
     return $data;
 }
 
-function checkPasswordStrength($password) {
-    $strength = 0;
-    if (strlen($password) >= 8) {
-        $strength++;
-    }
-    if (preg_match('/[a-z]/', $password) && preg_match('/[A-Z]/', $password)) {
-        $strength++;
-    }
-    if (preg_match('/\d/', $password)) {
-        $strength++;
-    }
-    if (preg_match('/[!@#$%^&*()_+{}[\]:;<>,.?~\\-]/', $password)) {
-        $strength++;
-    }
-    return $strength;
-}
+
 
 ?>
+
 
 <div class="container">
     <form action="#" method="POST">
         <div class="form-group">
             <label for="name">First name:</label>
-            <input type="text" class="form-control" placeholder="Enter name" name="name">
+            <input type="text" class="form-control" placeholder="Enter name" name="name" value="<?php echo $name; ?>">
             <span class="error"><?php echo $nameErr; ?></span>
         </div>
         <div class="form-group">
             <label for="lastname">Last name:</label>
-            <input type="text" class="form-control" placeholder="Enter last name" name="lastname">
+            <input type="text" class="form-control" placeholder="Enter last name" name="lastname"value="<?php echo $lastname; ?>">
             <span class="error"><?php echo $lastnameErr; ?></span>
         </div>
         <div class="form-group">
             <label for="email">Email address:</label>
-            <input type="email" class="form-control" placeholder="Enter email" name="email">
+            <input type="email" class="form-control" placeholder="Enter email" name="email"value="<?php echo $email; ?>">
             <span class="error"><?php echo $emailErr; ?></span>
         </div>
         <div class="form-group">
             <label for="password">Password:</label>
-            <input type="password" class="form-control" placeholder="Enter password" name="password">
+            <input type="password" class="form-control" placeholder="Enter password" name="password"value="<?php echo $password; ?>">
             <span class="error"><?php echo $passwordErr; ?></span>
+            
         </div>
         <button type="submit" name="submit" class="btn btn-primary">Submit</button>
     </form>
